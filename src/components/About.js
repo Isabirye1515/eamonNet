a7ee205f13c8bfd42032f3cab91764837bb738dd
Isabirye1515 <isabiryeelijah15@gmail.com>
2024-09-20 20:48:04 +0300

    others changin

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className='faqs' >
       <h1 style={{color:"#aaa ",marginLeft:"3%"}} >Our Story</h1>
-      <h1 style={{color:"#fff ",marginLeft:"3%"}}  >Welcome to  EAMON DREADS SALON, where passion meets expertise. Our team of skilled locticians is dedicated to providing exceptional dreadlocks services...</h1>
+      <h1 style={{color:"#0056b3  ",marginLeft:"3%"}}  >Welcome to  EAMON DREADS SALON, where passion meets expertise. Our team of skilled locticians is dedicated to providing exceptional dreadlocks services...</h1>
     
     </div>
   )
